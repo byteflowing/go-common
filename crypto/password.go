c8459dc59ee022f4ab99d3000a390740e6d925a7
only <richard@yanrongliang.com>
2025-08-07 22:55:15 +0800

    feat: 增加PasswordHasher

---
+++ crypto/password.go
@@ -0,0 +1,49 @@
+package crypto
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var DefaultPasswordHasher *PasswordHasher
+
+func init() {
+	DefaultPasswordHasher = NewPasswordHasher(bcrypt.DefaultCost)
+}
+
+type PasswordHasher struct {
+	cost int
+}
+
+func NewPasswordHasher(cost int) *PasswordHasher {
+	if cost < int(bcrypt.MinCost) || cost > int(bcrypt.MaxCost) {
+		cost = int(bcrypt.DefaultCost)
+	}
+	return &PasswordHasher{cost: cost}
+}
+
+func (ph *PasswordHasher) HashPassword(password string) (string, error) {
+	if password == "" {
+		return "", errors.New("empty password")
+	}
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func (ph *PasswordHasher) VerifyPassword(password, hash string) (bool, error) {
+	if password == "" || hash == "" {
+		return false, errors.New("empty password or hash")
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
