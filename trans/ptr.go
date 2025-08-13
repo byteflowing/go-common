b2bc3355eefac86858e0cfc95eea971d151a86f8
only <richard@yanrongliang.com>
2025-08-13 16:15:07 +0800

    feat: 增加Ref函数

--- trans/ptr.go
+++ trans/ptr.go
@@ -1,9 +1,13 @@
 package trans
 
-func DerefGeneric[T any](ptr *T) T {
+func Deref[T any](ptr *T) T {
 	if ptr == nil {
 		var zero T
 		return zero
 	}
 	return *ptr
+}
+
+func Ref[T any](v T) *T {
+	return &v
 }
